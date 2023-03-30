75861fef9406b221bf7d7994ec97f34385d823eb
Federico Bertolini <5268372+zabuTNT@users.noreply.github.com>
2023-03-30 22:36:43 +0200

    Create package-info.java

---
+++ package-info.java
@@ -0,0 +1,9 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://www.siri.org.uk/siri", 
+elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED,
+xmlns = {
+		@XmlNs(prefix="siri", namespaceURI="http://www.siri.org.uk/siri"),
+		@XmlNs(prefix="ojp", namespaceURI="http://www.vdv.de/ojp")
+})
+package com.bliksemlabs.ojp.model;
+
+import javax.xml.bind.annotation.XmlNs;
