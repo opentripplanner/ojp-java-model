fc99900551e4aa5ae9255d912a6ab0818e0ff89f
zabuTNT <f.bertolini@riseup.net>
2024-04-18 16:43:41 +0200

    restore previous location

---
+++ package-info.java
@@ -0,0 +1,15 @@
+@jakarta.xml.bind.annotation.XmlSchema(
+    namespace = "http://www.siri.org.uk/siri",
+    elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED,
+    xmlns = {
+        @jakarta.xml.bind.annotation.XmlNs(prefix = "ojp", namespaceURI = "http://www.vdv.de/ojp"),
+        @jakarta.xml.bind.annotation.XmlNs(prefix = "ifopt", namespaceURI = "http://www.ifopt.org.uk/ifopt"),
+        @jakarta.xml.bind.annotation.XmlNs(prefix = "acsb", namespaceURI = "http://www.ifopt.org.uk/acsb"),
+        @jakarta.xml.bind.annotation.XmlNs(prefix = "siri", namespaceURI = "http://www.siri.org.uk/siri")
+    }
+)
+package de.vdv.ojp.model;
+
+import jakarta.xml.bind.annotation.XmlNs;
+import jakarta.xml.bind.annotation.XmlNsForm;
+import jakarta.xml.bind.annotation.XmlSchema;
