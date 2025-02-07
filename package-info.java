ee5cbd589c29bdee0db7825ab5e0ad6e31639f9c
Leonard Ehrenfried <mail@leonard.io>
2025-02-07 12:15:23 +0100

    Configure nice namespaces

--- package-info.java
+++ package-info.java
@@ -1,14 +1,16 @@
 @jakarta.xml.bind.annotation.XmlSchema(
-    namespace = "http://www.siri.org.uk/siri",
+    namespace = "http://www.vdv.de/ojp",
     elementFormDefault = jakarta.xml.bind.annotation.XmlNsForm.QUALIFIED,
     xmlns = {
         @jakarta.xml.bind.annotation.XmlNs(prefix = "ojp", namespaceURI = "http://www.vdv.de/ojp"),
         @jakarta.xml.bind.annotation.XmlNs(prefix = "ifopt", namespaceURI = "http://www.ifopt.org.uk/ifopt"),
         @jakarta.xml.bind.annotation.XmlNs(prefix = "acsb", namespaceURI = "http://www.ifopt.org.uk/acsb"),
-        @jakarta.xml.bind.annotation.XmlNs(prefix = "siri", namespaceURI = "http://www.siri.org.uk/siri")
+        @jakarta.xml.bind.annotation.XmlNs(prefix = "siri", namespaceURI = "http://www.siri.org.uk/siri"),
+        @jakarta.xml.bind.annotation.XmlNs(prefix = "netex", namespaceURI = "http://www.netex.org.uk/netex"),
+        @jakarta.xml.bind.annotation.XmlNs(prefix = "opengis", namespaceURI = "http://www.opengis.net/gml/3.2"),
     }
 )
-package de.vdv.ojp.model;
+package de.vdv.ojp20.model;
 
 import jakarta.xml.bind.annotation.XmlNs;
 import jakarta.xml.bind.annotation.XmlNsForm;
